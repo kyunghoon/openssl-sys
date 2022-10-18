8f7826af6285b0b475570feda4b1f2400c29dfc0
Kyunghoon <kyunghoon@gmail.com>
2022-10-19 00:09:42 +0900

    compiles on android again

--- build.rs
+++ build.rs
@@ -1,21 +1,31 @@
 use anyhow::Result;
 use std::{process::Command, path::Path};
 
+macro_rules! check {
+    ($out: expr) => {
+        let out = $out;
+        if !out.status.success() {
+            panic!("{}", std::str::from_utf8(&out.stderr)?);
+        }
+    };
+}
+
 fn go() -> Result<()> {
     match std::env::var("TARGET") {
         Ok(target) if target == "aarch64-linux-android" => {
             match std::env::var("HOST") {
                 Ok(host) if host == "x86_64-apple-darwin" => {
+
                     let out_dir = std::env::var("OUT_DIR").unwrap();
-                    //let api_level = std::env::var("API_LEVEL").unwrap_or("23".to_owned());
-                    let api_level = std::env::var("API_LEVEL").unwrap_or("24".to_owned());
                     let ndk_root = std::env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME is undefined");
-                    
-                    let toolchain = format!("{}/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin", ndk_root);
+                    let api_level = std::env::var("API_LEVEL").unwrap_or("23".to_owned());
+
+                    let toolchain = format!("{}/toolchains/{}-4.9/prebuilt/darwin-x86_64/bin", ndk_root, target);
                     assert!(Path::new(toolchain.as_str()).exists(), "toolchain not found");
 
-                    Command::new("./Configure")
-                        .env("PATH", format!("{}:{}", toolchain.as_str(), env!("PATH")))
+                    check!(Command::new("./Configure")
+                        .env("PATH", format!("{}:{}", toolchain, env!("PATH")))
+                        .env("ANDROID_NDK", ndk_root.as_str())
                         .arg("android-arm64")
                         .arg("-fPIC")
                         .arg(format!("-D__ANDROID_API__={}", api_level))
@@ -23,17 +33,19 @@ fn go() -> Result<()> {
                         .arg("no-asm")
                         .arg("no-shared")
                         .arg("no-unit-test")
-                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?;
+                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?);
 
-                    Command::new("make")
-                        .env("PATH", format!("{}:{}", toolchain.as_str(), env!("PATH")))
-                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?;
+                    check!(Command::new("make")
+                        .env("PATH", format!("{}:{}", toolchain, env!("PATH")))
+                        .env("ANDROID_NDK", ndk_root.as_str())
+                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?);
 
-                    Command::new("make")
-                        .env("PATH", format!("{}:{}", toolchain.as_str(), env!("PATH")))
-                        .arg("install_sw")
+                    check!(Command::new("make")
+                        .env("PATH", format!("{}:{}", toolchain, env!("PATH")))
+                        .env("ANDROID_NDK", ndk_root.as_str())
                         .arg(format!("DESTDIR={}", out_dir))
-                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?;
+                        .arg("install_sw")
+                        .current_dir(format!("{}/openssl", env!("CARGO_MANIFEST_DIR"))).output()?);
 
                     println!("cargo:INCLUDE={}/usr/local/include", out_dir);
                     println!("cargo:LIB={}/usr/local/lib", out_dir);
